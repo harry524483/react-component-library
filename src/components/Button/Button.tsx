8107649641350f3b0cf830ff96f46e66a5c2429c
harry524483 <harinder.singh@sainsburys.co.uk>
2021-12-12 21:13:01 +0000

    feat: initial setup

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type ButtonProps = {
+  label: string;
+};
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
+
+export default Button;
